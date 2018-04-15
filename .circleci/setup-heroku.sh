74b016dec121cf38b384e7401553c2f08fbfc94a
takedajs <you@example.com>
2018-04-15 19:49:19 +0900

    no message

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git remote add heroku https://git.heroku.com/CodeIgniter_CircleCI.git
+git remote add heroku https://git.heroku.com/nameless-waters-42650.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
 tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
